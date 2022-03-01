f5ab27edec29666ead8dcc8668e2a410ad5d4364
willian <wwltongji@163.com>
2022-03-01 15:32:41 +0800

    texture added

--- TexManager.cpp
+++ TexManager.cpp
@@ -4,6 +4,5 @@ SDL_Texture* TexManager::LoadTexture(const char* texture, SDL_Renderer* ren)
 	SDL_Surface* tempSurface = IMG_Load(texture);
 	SDL_Texture* tex = SDL_CreateTextureFromSurface(ren,tempSurface);
 	SDL_FreeSurface(tempSurface);
-
 	return tex;
 }
